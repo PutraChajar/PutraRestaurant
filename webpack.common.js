b801c79ea6a3ad59a7a0bcb827e3861ff73c7dec
putra <puic@ubslinux.com>
2021-01-19 09:57:16 +0700

    add webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'images/',
+						}
+					}
+				]
+			}
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src/templates/index.html'),
+			favicon: "./src/public/images/favicon.ico",
+      filename: 'index.html',
+      title: 'Progressive Web Application',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
+    }),
+  ],
+};
